4884f658a824ddb571e2a0d4a2b65729c433c701
mail2.rameshr@gmail.com <mail2.rameshr@gmail.com>
2015-10-28 15:20:04 +0530

    Initial commit for thrift based clustering

---
+++ arithmetic.thrift
@@ -0,0 +1,7 @@
+namespace cpp tutorial.arithmetic.gen  
+namespace py bench 
+
+service ArithmeticService {  // defines simple arithmetic service
+            i64 add(1:i32 num1, 2:i32 num2),
+            i64 multiply(1:i32 num1, 2:i32 num2),
+}
